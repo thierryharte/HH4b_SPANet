f42af5b0f092c3d7c7c0580afcbdf7c49891313a
matteomalucchi <matteomalucchi13@gmail.com>
2024-04-05 16:12:51 +0200

    minor

--- jobs/submit_jobs_seed.sh
+++ jobs/submit_jobs_seed.sh
@@ -9,10 +9,12 @@ SEED_START=$2
 SEED_END=$3
 dir_name=$(basename $OPTION .json)
 
-ADDITIONAL_ARGS=""
+ADDITIONAL_ARGS="${@:4}"
+echo $ADDITIONAL_ARGS
 if [ $# -gt 3 ]; then
-    ADDITIONAL_ARGS="--args ${@:4}"
+    ADDITIONAL_ARGS="--args ${ADDITIONAL_ARGS}"
 fi
+echo $ADDITIONAL_ARGS
 
 # if SEED_START is not None, do the loop over the seeds
 if [ ! -z "$SEED_END" ]; then
