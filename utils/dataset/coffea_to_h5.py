1d0001e17c09412e3aed9ee139cbce430ca57c3f
matteomalucchi <matteomalucchi13@gmail.com>
2024-04-11 17:57:55 +0200

    add configs for k_lambda training

--- utils/dataset/coffea_to_h5.py
+++ utils/dataset/coffea_to_h5.py
@@ -44,7 +44,7 @@ out_dir = args.output if args.output else os.path.dirname(args.input)
 coffea_to_parquet = f"python coffea_to_parquet.py -i {args.input} -o {os.path.dirname(args.input)} -c {args.cat}"
 #subprocess.run(coffea_to_parquet, shell=True)
 
-parquet_to_h5 = f"python parquet_to_h5.py -i {os.path.dirname(args.input)}/{args.dataset}.parquet -o {out_dir} -f {args.frac_train} -s {args.no_shuffle}"
+parquet_to_h5 = f"python parquet_to_h5.py -i {os.path.dirname(args.input)}/{args.dataset}.parquet -o {out_dir} -f {args.frac_train} {'--no-shuffle' if args.no_shuffle else ''}"
 #subprocess.run(parquet_to_h5, shell=True)
 
 total_command=f"{coffea_to_parquet} && {parquet_to_h5}"
