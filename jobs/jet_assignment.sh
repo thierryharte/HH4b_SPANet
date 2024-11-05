e17e485d0f32488b9a701feacda620466c7389f9
Thierry Guillaume Harte <thierry.guillaume.harte@cern.ch>
2024-11-05 11:22:31 +0100

    resolved merge conflict

--- jobs/jet_assignment.sh
+++ jobs/jet_assignment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-SPANET_DIR=$HOME/SPANet
-HH4b_SPANET_DIR=$HOME/HH4b_SPANet
+SPANET_DIR=$HOME/public/Software/SPANet
+HH4b_SPANET_DIR=$HOME/public/Software/HH4b_SPANet
 NUM_GPU=1
 
 # Create venv in local job dir
@@ -35,3 +35,12 @@ else
            --gpus $NUM_GPU\
            "${@:4}"
 fi
+# elif [ $# -eq 5 ]; then
+#     python -m spanet.train \
+#            --options_file $1 \
+#            -n $2 \
+#            --log_dir $2\
+#            --checkpoint $4\
+#            --time_limit 07:00:00:00\
+#            --gpus $NUM_GPU\
+#            "${@:5}"
