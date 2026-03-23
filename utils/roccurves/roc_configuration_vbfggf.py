12c7015f958a8466be2ec21804f11a54d5cc1f15
neskongarr <neskongarr@gmail.com>
2026-03-23 16:06:41 +0100

    adding option files for nkontaxa

--- utils/roccurves/roc_configuration_vbfggf.py
+++ utils/roccurves/roc_configuration_vbfggf.py
@@ -42,3 +42,94 @@ true_dict = {
         "vbf": True,
     },
 }
+
+
+# =============================================================================
+# (nkontaxa) MODIFICATIONS TO spanet_dict below
+# =============================================================================
+spanet_dir_nestor = "/eos/user/n/nkontaxa/semester_project/spanet_outputs/"
+true_dir_nestor = "/eos/user/m/mmalucch/spanet_infos/spanet_inputs/"
+
+spanet_dict.update({
+    ###AllKlambda 7 jets
+    ##f"{spanet_dir_nestor}vbf/predictions_allKlambda_7jets_JetTotalSPANETPadded.h5": {
+    ##    "file": f"{spanet_dir_nestor}vbf/predictions_allKlambda_7jets_JetTotalSPANETPadded.h5",
+    ##    "true": "vbf_jet_total_padded",
+    ##    "label": "SPANet 7 jets all_Kl_PtFlattened_training (nkontaxa)",
+    ##    "color": "royalblue",
+    ##    "vbf": True,
+    ##},
+    
+    #AllKlambda 9 jets
+    f"{spanet_dir_nestor}vbf/predictions_allKlambda.h5": {
+        "file": f"{spanet_dir_nestor}vbf/predictions_allKlambda.h5",
+        "true": "vbf_jet_total_padded",
+        "label": "SPANet 9 jets all_Klambda (nkontaxa)",
+        "color": "red",
+        "vbf": True,
+    },
+
+    #500 epochs all_Klambda 9 jets
+    f"{spanet_dir_nestor}vbf/predictions_allKlambda_500epochs.h5": {
+        "file": f"{spanet_dir_nestor}vbf/predictions_allKlambda_500epochs.h5",
+        "true": "vbf_jet_total_padded",
+        "label": "SPANet 9 jets all_Klambda 500 epochs (nkontaxa)",
+        "color": "black",
+        "vbf": True,
+    },
+    
+    ###allKlambda_DetaMjj_NGW_noextravars
+    ##f"{spanet_dir_nestor}vbf/predictions_allKlambda_DetaMjj_novars_NormGenWeights.h5": {
+    ##    "file": f"{spanet_dir_nestor}vbf/predictions_allKlambda_DetaMjj_novars_NormGenWeights.h5",
+    ##    "true": "vbf_jet_total_padded",
+    ##    "label": "SPANet DetaMjj NGW noextravars (nkontaxa)",
+    ##    "color": "black",
+    ##    "vbf": True,
+    ##},
+
+    ###allKlambda_DetaMjj_NNGW_noextravars
+    ##    f"{spanet_dir_nestor}vbf/predictions_allKlambda_DetaMjj_novars_Non-NormGenWeights.h5": {
+    ##        "file": f"{spanet_dir_nestor}vbf/predictions_allKlambda_DetaMjj_novars_Non-NormGenWeights.h5",
+    ##        "true": "vbf_jet_total_padded",
+    ##        "label": "SPANet DetaMjj NNGW noextravars (nkontaxa)",
+    ##        "color": "green",
+    ##        "vbf": True,
+    ##    },
+
+})
+
+true_dict.update({
+    #AllKlambda 7 jets
+    "vbf_jet_total_padded": {
+        "name": f"{true_dir_nestor}vbf/vbf_all_Klambda/7jets_JetTotalSPANetPadded_test.h5",
+        "klambda": "postEE",
+        "vbf": True,
+    },
+    
+    #AllKlambda 9 jets
+    "vbf_jet_total_padded": {
+        "name": f"{true_dir_nestor}vbf/vbf_all_Klambda/JetTotalSPANetPadded_kl_combined_EVENT_test.h5",
+        "klambda": "postEE",
+        "vbf": True,
+    },
+
+    #AllKlambda DetaMjj NNGW noextravars jets
+    "vbf_jet_total_padded": {
+        "name": f"{true_dir_nestor}vbf/vbf_all_Klambda_DetaMjj/SaveMjjDeta_JetTotalSPANetPadded_test.h5",
+        "klambda": "postEE",
+        "vbf": True,
+    },
+
+    #AllKlambda DetaMjj NGW noextravars jets
+    "vbf_jet_total_padded": {
+        "name": f"{true_dir_nestor}vbf/vbf_all_Klambda_DetaMjj/SaveMjjDeta_NormGenWeights_JetTotalSPANetPadded_test.h5",
+        "klambda": "postEE",
+        "vbf": True,
+    },
+    # Add more entries here...
+})
+
+# =============================================================================
+# END MODIFICATIONS SECTION
+# =============================================================================
+
